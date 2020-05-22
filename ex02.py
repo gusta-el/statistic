bebee0594c384202930b20b48c2440ae427f7240
Gustavo Cabrera Cirilo <42786056+gusta-el@users.noreply.github.com>
2020-05-22 20:29:46 -0300

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,34 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import stats
 from scipy.stats import variation
+
+def getLinhas(rol):
+    media = np.mean(rol)
+    mediana = np.median(rol)
+    amplitude = max(rol) - min(rol)
+    variancia = np.var(rol)
+    desvio_padrao = np.std(rol)
+    data = [round(media, 2), round(mediana, 2), round(amplitude, 2), round(variancia, 2), round(desvio_padrao, 2)]
+    return data
 
 QUESTION = '\033[1m'
 RESET = '\033[0m'
 ANSWER = '\033[94m'
 DARK  = '\033[0;30m'
-rol = [21, 23, 23, 24, 26, 26, 26, 27, 27, 27, 27, 27, 27, 28, 28, 28, 28, 29, 29, 29, 29, 29, 29, 30, 30, 30, 30, 30, 30, 30, 31, 31, 31, 32, 32, 32, 32, 33, 33, 33, 34, 34, 34, 34, 34, 36, 36, 37, 37, 39]
 
-media = np.mean(rol)
-mediana = np.median(rol)
-desvio_padrao = np.std(rol)
-moda = stats.mode(rol)[0]
-coeficiente_variacao = variation(rol, axis=0)
+rol = [1434, 3692, 4482, 4119, 1043, 4520, 432, 618, 4564, 4086, 4371, 2918, 141, 127, 929, 3130, 3174, 2181, 559, 940, 5123, 5674, 1423, 1316, 901, 943, 3041, 3870, 5564, 353, 1161, 1732, 3322, 8608, 659]
+eua_value = 8608
 
-print(DARK + QUESTION + "a) Calcule a média, desvio padrão, coeficiente de variação, moda e mediana:" + RESET)
-print(ANSWER + "Media:", round(media, 2))
-print("Desvio padrão:", round(desvio_padrao, 2))
-print("Moda:", moda[0])
-print("Mediana:", round(mediana, 2))
-print("Coeficiente de variação:", round(coeficiente_variacao*100, 2), "% \n")
+row_labels = ["Média", "Mediana", "Amplitude", "Variância", "Desvio Padrão"]
+col_labels = ["Com EUA", "Sem EUA"]
 
-print(DARK + QUESTION + "b) Construir o histograma dos dados apresentados: " + RESET)
-plt.hist(rol, 'doane', histtype='bar',  facecolor='blue', alpha = 0.5, edgecolor = "black")
-plt.xlabel('Milhares de $')
-plt.ylabel('Semanas')
-plt.title('Histograma de Amostra de Vendas de uma firma durante 50 semanas')
-plt.grid(True, color='#CCCCCC', linestyle='--', alpha = 0.5)
+com_eua = getLinhas(rol)
+rol.remove(eua_value)
+sem_eua = getLinhas(rol)
+
+table_data = [[0,0], [0,0], [0,0], [0,0], [0,0]]
+for x in range(0,5):
+    table_data[x] = [com_eua[x], sem_eua[x]]
+
+
+fig = plt.figure(dpi=65)
+ax = fig.add_subplot(1,1,1)
+table = ax.table(cellText=table_data, loc='center', rowLabels=row_labels, colLabels=col_labels)
+table.set_fontsize(14)
+table.scale(1,3)
+
+ax.axis('off')
+
+
+print(DARK + QUESTION + "Ache a média das despesas com cuidados da saúde nesses países, com e sem os Estados Unidos.")
+print("Faça os seguintes cálculos (use calculadora ou o computador):")
+
 plt.show()
 
-print(DARK + QUESTION + "c) Classificar o histograma em relação a simetria." + RESET)
-print(ANSWER + "Histograma simétrico")
+print(DARK + QUESTION + "Qual o impacto desse valor atípico?"+ RESET)
+print(ANSWER + "Este impacto faz com que valores como média e amplitude se alterem de forma significativa pois são muito influenciados por seus outliers.")
