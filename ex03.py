52e117250e8168adeec8e529de95e1f541479ce0
Gustavo Cabrera Cirilo <42786056+gusta-el@users.noreply.github.com>
2020-05-22 20:00:15 -0300

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -8,7 +8,7 @@ desvio_padrao = np.std(rol)
 moda = stats.mode(rol)[0]
 coeficiente_variacao = variation(rol, axis=0)
     
-print(DARK + QUESTION + "Um analista possui as seguintes informações a respeito dos valores a de uma amostra de 30 observações:"+ RESET)
+print(DARK + QUESTION + "3) Um analista possui as seguintes informações a respeito dos valores a de uma amostra de 30 observações:"+ RESET)
 print("- A média de todos os valores é igual a 2.96 ")
 print("- A soma dos quadrados dos valores é igual 268 Calcule o desvio padrão amostral")
 print(DARK + QUESTION + "Calcule o desvio padrão amostral. "+ RESET)
